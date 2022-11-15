52798823c4e62118b614ab8771caf97156df33be
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-15 18:49:35 -0500

    xcvcx

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+   /*     We want to write a program that asks the user for three values (a0, a1, a2) and displays
+        the actual solution (s) z of the third degree equation:
+      z3 + a2 z^2 + a1 z + a0 = 0  */
+
+        double a0 , a1 , a2;
+        Scanner sc = new Scanner()
+    }
+}
